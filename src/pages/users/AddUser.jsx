7aa3f4a87c65f1c63aa7b4c63b543de118572e6f
billy <billy.wongkar@team.cranium.id>
2023-11-09 15:31:24 +0700

    test
    test

--- src/pages/users/AddUser.jsx
+++ src/pages/users/AddUser.jsx
@@ -3,7 +3,7 @@ import Layout from '../utils/Layout';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { GetMe } from '../../features/authSlice';
-import AddUserForm from '../../components/Users/AddUserForm';
+import AddUserForm from '../../components/users/AddUserForm';
 
 const AddUser = () => {
   const dispatch = useDispatch();
