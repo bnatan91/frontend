18dbc15df99e03e99775270f292f038cac219d19
billy <billy.wongkar@team.cranium.id>
2023-11-09 15:37:53 +0700

    testte
    test

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:5000',
+      '/api': 'https://monkfish-app-hmwl9.ondigitalocean.app',
     },
   },
   plugins: [react()],
